d4adc5a2db7db021c7a9b84ecdbcb2e6c9e387f4
rock321987 <rock321987@users.noreply.github.com>
2014-05-12 09:34:49 +0530

    Create ATOMS.py

---
+++ ATOMS.py
@@ -0,0 +1,18 @@
+i = 1;
+ 
+t = input();
+ 
+while i <= t:
+	i += 1;
+	n, k , m = [int(x) for x in raw_input().split()];
+	res = n;
+	cnt = 0;
+	
+	while 1:
+		res *= k;
+		if res > m:
+			break;
+		else:
+			cnt += 1;
+			
+	print cnt 
