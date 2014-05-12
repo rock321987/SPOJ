628e80094c0fe6a552d388702939c23e4af249c5
rock321987 <rock321987@users.noreply.github.com>
2014-05-12 09:35:55 +0530

    Create CUTCAKE.cpp

---
+++ CUTCAKE.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cmath>
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	
+	while (t--) {
+		long long int n;
+		scanf("%lld", &n);
+		
+		printf("%0.0lf\n", (-1 + sqrt(1 + 8 * (n - 1))) / 2);
+	}
+	return 0;
+}
