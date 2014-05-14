320cfdb53b5b7577a4fe91caa71f9ea2d3c3324b
WA <rock321987@users.noreply.github.com>
2014-05-14 08:32:05 +0530

    http://www.spoj.com/problems/TOANDFRO/

---
+++ TOANDFRO.cpp
@@ -0,0 +1,58 @@
+/*
+  Ad-hoc..this is not an efficient solution..there are many other efficient method
+*/
+
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	while (1) {
+		int n, i, x, j;
+		scanf("%d", &n);
+		
+		if (n == 0) {
+			break;
+		}
+		
+		char str[100005], tmp[1];
+		scanf("%s", str);
+		
+		x = strlen(str) / n;
+		char arr[1005][1005];
+		//printf("%d	", x);
+		if (strlen(str) % n > 0) {
+			for (i = strlen(str); i < (x + 1)* n;i++) {
+				str[i] = 'x';
+			}
+			x++;
+		}
+		
+		for (i = 0;i < x;i++) {
+			for (j = 0;j < n;j++) {
+				arr[i][j] = str[i * n + j];
+			}
+		}
+		
+		for (i = 0;i < x;i++) {
+			if (i % 2 == 1) {
+				for (j = 0;j < n / 2;j++) {
+					tmp[0] = arr[i][j];
+					arr[i][j] = arr[i][n - j - 1];
+					arr[i][n - j - 1] = tmp[0];
+				}
+			}
+		}
+		
+		for (i = 0;i < n;i++) {
+			for (j = 0;j < x;j++) {
+				printf("%c", arr[j][i]);
+			}
+		}
+		printf("\n");
+		
+	}
+	return 0;
+}
