5affce8bbfdae0c936bc4f9a5ca6ba4573e894c0
WA <rock321987@users.noreply.github.com>
2014-07-25 01:13:37 +0530

    http://www.spoj.com/problems/CPRMT/

---
+++ CPRMT.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+class cprmt {
+	public:
+		void calculate()
+		{
+			char arr1[100005], arr2[100005];
+			
+			while ((scanf("%s%s", arr1, arr2)) != EOF) {
+				int freq1[256] = {0}, freq2[256] = {0};
+				
+				for (int i = 0;i < strlen(arr1);i++) {
+					freq1[arr1[i]]++;
+				}
+				
+				for (int i = 0;i < strlen(arr2);i++) {
+					freq2[arr2[i]]++;
+				}
+				
+				for (int i = 95;i < 130;i++) {
+					if (freq1[i] > 0 && freq2[i] > 0) {
+						for (int j = 0;j < (freq1[i] > freq2[i] ? freq2[i] : freq1[i]);j++) {
+							printf("%c", i);
+						}
+					}
+				}
+				printf("\n");
+			}
+		}
+};
+
+int main()
+{
+	cprmt obj;
+	obj.calculate();
+	
+	return 0;
+}
